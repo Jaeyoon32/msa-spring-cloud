105182b9956d48236ac71a3b34ff1f56194d7b44
Jaeyoon32 <jaeyoonlim@lguplus.co.kr>
2021-12-02 16:02:10 +0900

    initial commit for Spring cloud server for MSA prototype #2

---
+++ src/main/java/com/kidd/msasample/springcloudmsa/SpringCloudMsaApplication.java
@@ -0,0 +1,15 @@
+package com.kidd.msasample.springcloudmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMsaApplication.class, args);
+    }
+
+}
